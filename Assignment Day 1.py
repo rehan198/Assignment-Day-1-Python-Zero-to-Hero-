18d8e6db68d0c75468f1efef98954475c068fb48
rehan198 <90082194+rehan198@users.noreply.github.com>
2021-09-04 10:39:47 +0530

    Day 1 Assignment Submitted

---
+++ Assignment Day 1.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+Q1 Add two number
+# In[1]:
+
+
+8+9
+
+
+# In[2]:
+
+
+a = 45
+b = 56
+print(a+b)
+
+
+# In[ ]:
+
+
+
+
+Q2 Try to add integer with string and see the output
+# In[3]:
+
+
+g = 45
+h = "Hello World"
+print(g+h)         #here we cannot add integer with the string bcz both are different 
+                   # if we want to add the we have to convert integer into string or string into integer.
+
+
+# In[7]:
+
+
+l = str(45)
+h = ("Hello World:")
+print(l+h)
+
+
+# In[ ]:
+
+
+
+
+Q3 Print Variable and strings in one line with the help of f string
+# In[8]:
+
+
+marks = 10
+name = "rehan"
+age = 22
+print(f"The marks of {name} is {marks} and age is {age}")
+
+
+# In[ ]:
+
+
+
+
+Q4 How to add space and new line in the print statement
+# In[10]:
+
+
+print("hello\n Everyone")
+
+
+# In[9]:
+
+
+print("hello\t World")
+
+
+# In[ ]:
+
+
+
+
